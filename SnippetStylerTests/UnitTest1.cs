6b88f22781db29089c6d1b8cbede1e815af97c1c
CalGrimes <cal.grimes@outlook.com>
2023-10-19 21:00:41 +0100

    Setup TDD + Changed git root
    - Moved git root folder up 1 level
    - Setup project for testing the SnippetStyler project

---
+++ SnippetStylerTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SnippetStylerTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange : Setting up the test
+
+            // Act : Calling the method to be tested
+
+            // Assert : Verifying the result
+            
+        }
+    }
+}
